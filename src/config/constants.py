cbd8a10251c4377e52ec40d287982372a7e01523
Erland Grimstad <erland.grimstad@yahoo.no>
2022-06-27 15:42:40 +0200

    initial commit

---
+++ src/config/constants.py
@@ -0,0 +1,21 @@
+import os
+import torch as th
+from pathlib import Path
+
+
+CUDA_AVALABLE = th.cuda.is_available()
+DEVICE = th.device("cuda" if CUDA_AVALABLE else "cpu")
+
+PROJECT_ROOT = Path(os.path.abspath(__file__)).parents[2]
+DATA_DIR = PROJECT_ROOT / "data"
+MODELS_DIR = PROJECT_ROOT / "models"
+
+DATETIME_FMT = "%Y-%m-%d_%H-%M-%S"
+
+LABELS_STRING = {
+    0: "Pneumonia",
+    1: "Pneumothorax",
+    2: "Pleural effusion",
+    3: "Pulmonary nodule",
+    4: "Diaphragmatic hernia"
+}
